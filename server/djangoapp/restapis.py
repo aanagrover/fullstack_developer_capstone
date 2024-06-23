32a1aefe7c2eb27a639eda65e54b8a9bb3c06006
Aanchal Grover <42566348+aanagrover@users.noreply.github.com>
2024-06-23 12:35:52 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -18,7 +18,8 @@ sentiment_analyzer_url = os.getenv(
 #     print("GET from {} ".format(url))
 #     try:
 #         # Call get method of requests library with URL and parameters
-#         response = requests.get(url, headers={'Content-Type': 'application/json'},
+#         response = requests.get(url,
+# headers={'Content-Type': 'application/json'},
 #                                     params=kwargs)
 #     except:
 #         # If any error occurs
@@ -28,10 +29,11 @@ sentiment_analyzer_url = os.getenv(
 #     json_data = json.loads(response.text)
 #     return json_data
 
-def get_request(endpoint, **kwargs):
+
+def get_request(endpoint, **kwargs): 
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
+    if(kwargs): 
+        for key, value in kwargs.items(): 
             params=params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
@@ -41,45 +43,51 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e: 
         # If any error occurs
         print("Network exception occurred")
 
-def post_request(url,json_payload,**kwargs):
-    response = requests.post(url, params=kwargs, json=payload)
+
+def post_request(url, json_payload, **kwargs):
+    response = requests.post(url, params=kwargs, json=json_payload)
     return response
 
-def get_dealers_from_cf(url, **kwargs):
-    results = []
-    # Call get_request with a URL parameter
-    json_result = get_request(url)
-    if json_result:
-        # Get the row list in JSON as dealers
-        dealers = json_result["rows"]
-        # For each dealer object
-        for dealer in dealers:
-            # Get its content in `doc` object
-            dealer_doc = dealer["doc"]
-            # Create a CarDealer object with values in `doc` object
-            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
-                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                   short_name=dealer_doc["short_name"],
-                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
-            results.append(dealer_obj)
 
-    return results
+#---- commented as didn't find if used---
+# def get_dealers_from_cf(url, **kwargs):
+#     results = []
+#     # Call get_request with a URL parameter
+#     json_result = get_request(url)
+#     if json_result:
+#         # Get the row list in JSON as dealers
+#         dealers = json_result["rows"]
+#         # For each dealer object
+#         for dealer in dealers:
+#             # Get its content in `doc` object
+#             dealer_doc = dealer["doc"]
+#             # Create a CarDealer object with values in `doc` object
+#             dealer_obj = CarDealer(address=dealer_doc["address"],
+# city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+#                                    id=dealer_doc["id"],
+# lat=dealer_doc["lat"], long=dealer_doc["long"],
+#                                    short_name=dealer_doc["short_name"],
+#                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
+#             results.append(dealer_obj)
 
-def get_dealer_reviews_from_cf(url, dealerId):
-    results = []
-    # Call get_request with a URL parameter
-    json_result = get_request(url)
-    if json_result:
-        # Get the row list in JSON as dealers
-        dealers = json_result["rows"]
-    review_obj.sentiment = analyze_review_sentiments(review_obj.review)
+#     return results
+
+# def get_dealer_reviews_from_cf(url, dealerId):
+#     results = []
+#     # Call get_request with a URL parameter
+#     json_result = get_request(url)
+#     if json_result:
+#         # Get the row list in JSON as dealers
+#         dealers = json_result["rows"]
+#     review_obj.sentiment = analyze_review_sentiments(review_obj.review)
+
 
 # def analyze_review_sentiments(dealerreview):
-#     get_request(url,**kwargs)
+#     get_request(url, **kwargs)
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
@@ -101,6 +109,6 @@ def post_review(data_dict):
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as err:
         print("Network exception occurred")
 # Add code for posting review

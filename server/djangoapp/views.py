cb21d3dc4a8e1203d94276934c86b25ea84c27e2
Aanchal Grover <42566348+aanagrover@users.noreply.github.com>
2024-06-23 15:06:05 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -40,9 +40,9 @@ def login_user(request):
 
 
 # Create a `logout_request` view to handle sign out request
-def logout_request(request): 
+def logout_request(request):
     logout(request)
-    data = {"userName":""}
+    data = {"userName": ""}
     return JsonResponse(data)
 
 
@@ -62,52 +62,41 @@ def registration(request):
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except Exception as e: 
+    except Exception as e:
         # If not, simply log this is a new user
+        print(f"error {e}")
         logger.debug("{} is new user".format(username))
     # If it is a new user
-    if not username_exist: 
+    if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username,
-                    first_name=first_name, last_name=last_name,
-                    password=password, email=email)
+        user = User.objects.create_user(
+            username= username,
+            first_name= first_name,
+            last_name= last_name,
+            password= password,
+            email= email
+        )
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName": username,"status": "Authenticated"}
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName": username,"error": "Already Registered"}
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
-def get_cars(request): 
+def get_cars(request):
     print("inside function")
-    count= CarMake.objects.filter().count()
+    count = CarMake.objects.filter().count()
     print(count)
     if(count == 0):
         initiate()
     car_models= CarModel.objects.select_related('car_make')
-    cars= []
+    cars = []
     for car_model in car_models:
         cars.append({"CarModel": car_model.name,
                      "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
-
-# Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
-#--------------------
-# def get_dealerships(request):
-#     if request.method == "GET":
-#         url = "your-cloud-function-domain/dealerships/dealer-get"
-#         # Get dealers from the URL
-#         dealerships = get_dealers_from_cf(url)
-#         # Concat all dealer's short name
-#         dealer_names = ' '.join([dealer.short_name
-# for dealer in dealerships])
-#         # Return a list of dealer short name
-#         return HttpResponse(dealer_names)
+    return JsonResponse({"CarModels": cars})
 
 #Update the `get_dealerships` render list of dealerships
 # all by default, particular state if state is passed
@@ -117,7 +106,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint= "/fetchDealers/"+state
     dealerships= get_request(endpoint)
-    return JsonResponse({"status": 200,"dealers": dealerships})
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
@@ -137,25 +126,25 @@ def get_dealer_details(request, dealer_id):
 # def add_review(request, dealer_id):
 #     pass
 
-def get_dealer_reviews(request, dealer_id): 
+def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
-    if(dealer_id): 
-        endpoint= "/fetchReviews/dealer/"+str(dealer_id)
-        reviews= get_request(endpoint)
-        for review_detail in reviews: 
-            response= analyze_review_sentiments(review_detail['review'])
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment']= response['sentiment']
-        return JsonResponse({"status": 200,"reviews": reviews})
+        return JsonResponse({"status": 200, "reviews": reviews})
     else:
-        return JsonResponse({"status": 400,"message": "Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
 def add_review(request): 
     if(request.user.is_anonymous == False): 
         data= json.loads(request.body)
         try: 
-            response= post_review(data)
+            response = post_review(data)
             print(response)
             return JsonResponse({"status": 200})
         except Exception as e:

943939ef43c7b00d892dc305b0cccce94ff2e9f1
Aanchal Grover <42566348+aanagrover@users.noreply.github.com>
2024-06-23 15:19:51 -0400

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -36,7 +36,7 @@ def get_request(endpoint, **kwargs):
         for key, value in kwargs.items():
             params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
@@ -93,7 +93,7 @@ def post_request(url, json_payload, **kwargs):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -105,7 +105,7 @@ def analyze_review_sentiments(text):
 
 # def post_review(data_dict):
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
